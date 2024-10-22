ddea33c30d3ca2fced2f0d3b558e2f4bf36b3235
jessicab09 <neni1509@outlook.com>
2024-10-22 18:04:44 -0400

    added new function related to final project

--- python_1/functions.py
+++ python_1/functions.py
@@ -1,5 +1,10 @@
-def wifi_creds():
+def wifi_creds(): # defines the function
     print("Name: public wifi")
     print("Password: wifipassword123")
     
-wifi_creds()
+wifi_creds() # function to tell wifi credentials
+
+def weatherToday():
+    print("The weather today is sunny and warm.")
+    
+    weatherToday() # function for telling the weather today
