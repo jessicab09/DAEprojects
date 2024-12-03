6ebd7db687d323a8bb2f7723f8b396a008787fbb
jenn6s <vibertsingh1158@gmail.com>
2024-12-03 18:15:28 -0500

    Finished prompt engineering and python

--- python_1/while_loop.py
+++ python_1/while_loop.py
@@ -1,7 +1,15 @@
-# while loop
+# while loop that loops thorught numbers 1-20
 print("count to 20")
 currentNumber = 1
 endNumber = 21
+
+list_of_colors = [ "Orange, Red, Yellow, Green, Blue, Purple, Pink" ]
+
 while currentNumber < endNumber:
     print(currentNumber, "mississipi")
     currentNumber = currentNumber + 1
+    
+print( "Here are my colorss" )
+
+for eachColor in list_of_colors:
+    print( eachColor )
